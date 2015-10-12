958383fd2f90841bd58baa75a5ed3c0c9ba865cd
Mikk Kärner <k2rnermikk@gmail.com>
2015-10-12 12:12:14 +0300

    crackpush

--- MD5cracker.py
+++ MD5cracker.py
@@ -1,55 +1,61 @@
 import hashlib
 
-wildcard=" "
 
-def main():
-	print("MD5 cracker is about to start working!")
-	tocrack="0d61f8370cad1d412f80b84d143e1257" # hash of C
-	tocrack="5332ba558c6706d24ae90e9ffdbdac1f" # hash of aebgc
-	tocrack="68e1c85222192b83c04c0bae564b493d" # hash of koer 
-	result=md5_crack(tocrack,"k  r") # Add input string to crack instead of "k  r"
-	if result:
-		print("Cracking "+tocrack+" gave "+result)
-	else:
-		print("Failed to crack "+tocrack)
+# wildcard = " "
 
-def md5_crack(hexhash,template): 
-	# Instantiate template and crack all instantiations
-	# First block recursively instantiates template
-	i=0
-	found=False
-	#print(wildcard + " - this is the wildcard!")
-	#print(len(template))
-	while i<len(template):
-		#print("While is running!")
-		#print(template)
-		#print(template[i])
-		if template[i] == wildcard:
-			#print("Got here!!!")
-			found=True
-			char=32 # Start with this char ascii
-			while char<126:
-				c=str(chr(char))
-				if c!=wildcard: # Cannot check wildcard!
-					ntemplate=template[:i]+c+template[i+1:]
-					#print("i: "+str(i)+" ntemplate: "+ntemplate)
-					result=md5_crack(hexhash,ntemplate)
-					if result: # Stop immediately if cracked
-						return result
-				char+=1
-		i+=1
-	# Instantiation loop done
-	#print("Why did I get here?")
-	if not found:
-		# No wildcards found in template: crack
-		m=hashlib.md5()
-		m.update(template.encode('ascii'))
-		hash=m.hexdigest()
-		#print("Template: "+template+"\nHash: "+hash) 
-		if hash==hexhash: 
-			print("I found it!")
-			return template # Cracked!
-	# Template contains wildcards
-	return None
 
-main()
+class MD5Cracker:
+    # def main():
+    # 	print("MD5 cracker is about to start working!")
+    # 	tocrack = "0d61f8370cad1d412f80b84d143e1257"  # hash of C
+    # 	tocrack = "5332ba558c6706d24ae90e9ffdbdac1f"  # hash of aebgc
+    # 	tocrack = "68e1c85222192b83c04c0bae564b493d"  # hash of koer
+    # 	result = md5_crack(tocrack, "k  r")  # Add input string to crack instead of "k  r"
+    # 	if result:
+    # 		print("Cracking " + tocrack + " gave " + result)
+    # 	else:
+    # 		print("Failed to crack " + tocrack)
+
+    def md5_crack(self, hexhash, template, wildcard):
+        # Instantiate template and crack all instantiations
+        # First block recursively instantiates template
+        i = 0
+        found = False
+        # print(wildcard + " - this is the wildcard!")
+        # print(len(template))
+        while i < len(template):
+            # print("While is running!")
+            # print(template)
+            # print(template[i])
+            if template[i] == wildcard:
+                # print("Got here!!!")
+                found = True
+                char = 32  # Start with this char ascii
+                while char < 126:
+                    c = str(chr(char))
+                    if c != wildcard:  # Cannot check wildcard!
+                        ntemplate = template[:i] + c + template[i + 1:]
+                        # print("i: "+str(i)+" ntemplate: "+ntemplate)
+                        result = self.md5_crack(hexhash, ntemplate, wildcard)
+                        if result:  # Stop immediately if cracked
+                            return result
+                    char += 1
+            i += 1
+        # Instantiation loop done
+        # print("Why did I get here?")
+        if not found:
+            # No wildcards found in template: crack
+            m = hashlib.md5()
+            m.update(template.encode('ascii'))
+            hash = m.hexdigest()
+            # print("Template: "+template+"\nHash: "+hash)
+            if hash == hexhash:
+                print("I found it!")
+                return template  # Cracked!
+        # Template contains wildcards
+        return None
+
+# if __name__ == '__main__':
+#     cracker = MD5Cracker()
+#     c = cracker.md5_crack('68e1c85222192b83c04c0bae564b493d', 'koer', '?')
+#     print(c)
--- handler.py
+++ handler.py
@@ -3,8 +3,10 @@ from urllib.parse import parse_qs, urlparse
 from utilis import delay
 from machines import Machines
 from request_sender import RequestSender
+from MD5cracker import MD5Cracker
 import socket
 import json
+
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -26,17 +28,18 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.hash = query['md5']
                 self.machines = Machines.get_machines()
 
-                q = {'sendip': '127.0.0.1', 'sendport': str(self.server.server_port), 'ttl': '4',
-                     'id': 'kuusepuukuller'}
-                # q['sendip'] = str(socket.gethostbyname(socket.gethostname()))
-
                 self.wfile.write(bytes('Starting calculations\n', 'UTF-8'))
+
+                q = {'sendip': '127.0.0.1', 'sendport': str(self.server.server_port), 'ttl': '4',
+                     'id': 'kuusepuukuller', 'noask': '127.0.0.1_' + str(self.server.server_port)}
+                # q['sendip'] = str(socket.gethostbyname(socket.gethostname()))
 
                 for machine in self.machines:
                     dest_ip = machine[0]
                     dest_port = machine[1]
                     if dest_ip == q['sendip'] and dest_port == q['sendport']:
                         continue
+
                     RequestSender.make_resource_request(dest_ip, dest_port, q)
 
                 self.send_assignments()
@@ -116,9 +119,20 @@ class MyHandler(BaseHTTPRequestHandler):
             data = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
             json_data = json.loads(data)
 
-            # TODO: calculada md5, given the range and symbolrange in json_data
-            resutl = 'sitaxcoolparool'
-            result_code = 0 # 0 - match found, 1 - not found
+            # TODO: calculada md5, given the 'ranges' and 'symbolrange' in json_data
+            ranges = json_data['ranges']
+            cracker = MD5Cracker()
+            # for trange in ranges:
+            #     result = cracker.md5_crack(json_data['md5'], trange, json_data['wildcard'])
+            #     if result:
+            #         result_code = 0 # 0 - match found, 1 - not found
+            #         break
+            #     else:
+            #         result_code = 1
+            #         result = '------NONE FOUND------'
+
+            result='asd'
+            result_code = 0
 
             my_ip = socket.gethostbyname(socket.gethostname())
             my_port = self.server.server_port
@@ -126,7 +140,7 @@ class MyHandler(BaseHTTPRequestHandler):
             # for local testing
             my_ip = '127.0.0.1'
 
-            RequestSender.send_md5_result(json_data['ip'], json_data['port'], my_ip, my_port, 'idjo', self.hash, result_code, resutl)
+            RequestSender.send_md5_result(json_data['ip'], json_data['port'], my_ip, my_port, 'idjo', self.hash, result_code, result)
 
         if url.path == '/answermd5':
             self.send_response(200)
@@ -170,7 +184,7 @@ class MyHandler(BaseHTTPRequestHandler):
 
             md5data = {
                 'md5': self.hash,
-                'ranges': ['asd','dsa','???'], # add real ranges later
+                'ranges': ['ko??','','???'], # add real ranges later
                 'wildcard': '?', # add real wildcard later
                 'symbolrange': [[3, 10], [100, 150]] # add real symbolrange later
             }
